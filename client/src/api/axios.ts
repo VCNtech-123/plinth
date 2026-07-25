3ddeb9542effa6a49463761a9a1a6daa56396c9a
VCNtech-123 <johnvecina640@gmail.com>
2026-07-26 02:35:18 +0800

    feat: enhance axios instance with response interceptor for 401 handling

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,9 +1,22 @@
 import axios from "axios";
+import type { AxiosInstance } from 'axios'
 
-export const api = axios.create({
-    baseURL: "http://localhost:5000/api",
-    withCredentials: true
+export const api: AxiosInstance = axios.create({
+  baseURL: "http://localhost:5000/api",
+  withCredentials: true,
 });
 
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response?.status === 401) {
+      const isAlreadyOnLogin = window.location.pathname === "/login";
+      
+      if (!isAlreadyOnLogin) {
+        window.location.href = "/login";
+      }
+    }
 
-
+    return Promise.reject(error);
+  }
+);
