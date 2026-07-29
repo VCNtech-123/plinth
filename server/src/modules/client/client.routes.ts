0e1af789c7a014611f8dc8e3b5bc6d8835e42cc0
VCNtech-123 <johnvecina640@gmail.com>
2026-07-30 04:59:56 +0800

    refactor: enhance getClientById route to use zod validation schema

--- server/src/modules/client/client.routes.ts
+++ server/src/modules/client/client.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { protect } from "../../middleware/auth.middleware";
 import { createClient } from "./client.controller";
-import { validateCreateClient } from "./client.validation";
+import { validateCreateClient, getClientByIdSchema } from "./client.validation";
 import { getClients, getClientById, updateClient, deleteClient } from './client.controller'
+import { validate } from "../../middleware/validate.middleware";
 
 const router = Router();
 
 router.post("/", protect, validateCreateClient, createClient);
 router.get("/", protect, getClients);
-router.get("/:id", protect, getClientById);
+router.get("/:id", protect, validate(getClientByIdSchema),getClientById);
 router.put("/:id", protect, updateClient);
 router.delete("/:id", protect, deleteClient);
+
 export default router;
