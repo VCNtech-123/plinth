2ace7a06c6974c0c2d2ef432d643afac1e47f7d1
VCNtech-123 <johnvecina640@gmail.com>
2026-07-31 14:44:50 +0800

    refactor: update client routes to use createClientSchema for validation

--- server/src/modules/client/client.routes.ts
+++ server/src/modules/client/client.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { protect } from "../../middleware/auth.middleware";
 import { createClient } from "./client.controller";
-import { validateCreateClient, getClientByIdSchema } from "./client.validation";
+import { createClientSchema, getClientByIdSchema } from "./client.validation";
 import { getClients, getClientById, updateClient, deleteClient } from './client.controller'
 import { validate } from "../../middleware/validate.middleware";
 
 const router = Router();
 
-router.post("/", protect, validateCreateClient, createClient);
+router.post("/", protect, validate(createClientSchema), createClient);
 router.get("/", protect, getClients);
-router.get("/:id", protect, validate(getClientByIdSchema),getClientById);
+router.get("/:id", protect, validate(getClientByIdSchema), getClientById);
 router.put("/:id", protect, updateClient);
 router.delete("/:id", protect, deleteClient);
 
