b8e8818e21a3d75625a48f1ce67df43926f60e51
VCNtech-123 <johnvecina640@gmail.com>
2026-07-31 19:38:09 +0800

    refactor: integrate updateClientSchema for improved project update validation

--- server/src/modules/project/project.routes.ts
+++ server/src/modules/project/project.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { protect } from '../../middleware/auth.middleware';
 import { createProject, getProjectById, getProjects, updateProject, deleteProject, restoreProject } from './project.controller';
-import { createProjectSchema, getProjectSchema, getProjectByIdSchema } from './project.validation';
+import { createProjectSchema, getProjectSchema, getProjectByIdSchema, updateClientSchema } from './project.validation';
 import { validate } from '../../middleware/validate.middleware';
 
 
@@ -10,7 +10,7 @@ const router = Router();
 router.post("/", protect, validate(createProjectSchema), createProject);
 router.get("/", protect, validate(getProjectSchema), getProjects);
 router.get("/:id", protect, validate(getProjectByIdSchema), getProjectById);
-router.put("/:id", protect, updateProject);
+router.put("/:id", protect, validate(updateClientSchema), updateProject);
 router.delete("/:id", protect, deleteProject);
 router.patch("/:id/restore", protect, restoreProject)
 
