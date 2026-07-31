6eed3328a0d693c148bced1626a3e65e4905efb6
VCNtech-123 <johnvecina640@gmail.com>
2026-07-31 19:42:16 +0800

    refactor: add validation schemas for delete and restore project routes

--- server/src/modules/project/project.routes.ts
+++ server/src/modules/project/project.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { protect } from '../../middleware/auth.middleware';
 import { createProject, getProjectById, getProjects, updateProject, deleteProject, restoreProject } from './project.controller';
-import { createProjectSchema, getProjectsSchema, getProjectByIdSchema, updateProjectSchema } from './project.validation';
+import { createProjectSchema, getProjectsSchema, getProjectByIdSchema, updateProjectSchema, deleteProjectSchema, restoreProjectSchema } from './project.validation';
 import { validate } from '../../middleware/validate.middleware';
 
 
@@ -11,8 +11,8 @@ router.post("/", protect, validate(createProjectSchema), createProject);
 router.get("/", protect, validate(getProjectsSchema), getProjects);
 router.get("/:id", protect, validate(getProjectByIdSchema), getProjectById);
 router.put("/:id", protect, validate(updateProjectSchema), updateProject);
-router.delete("/:id", protect, deleteProject);
-router.patch("/:id/restore", protect, restoreProject)
+router.delete("/:id", protect, validate(deleteProjectSchema), deleteProject);
+router.patch("/:id/restore", protect, validate(restoreProjectSchema), restoreProject)
 
 export default router;
 
