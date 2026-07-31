93f58abe6011c93fd71976f0d00f8b4a131bd662
VCNtech-123 <johnvecina640@gmail.com>
2026-07-31 21:04:49 +0800

    refactor: replace validateCreateTask with validate using createProjectSchema for task creation

--- server/src/modules/task/task.routes.ts
+++ server/src/modules/task/task.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { protect } from '../../middleware/auth.middleware';
-import { validateCreateTask } from './task.validation';
+import { createProjectSchema } from './task.validation';
 import { createTask, getTasks, getTaskById, updateTaskById, deleteTask, restoreTask } from './task.controller';
+import { validate } from '../../middleware/validate.middleware';
 
 const router = Router();
 
-router.post("/", protect, validateCreateTask, createTask );
+router.post("/", protect, validate(createProjectSchema), createTask );
 router.get("/", protect, getTasks);
 router.get("/:id", protect, getTaskById);
 router.put("/:id", protect, updateTaskById);
