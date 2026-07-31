84a936d64bcb75a766151c1efd0c3ac2d4f82063
VCNtech-123 <johnvecina640@gmail.com>
2026-07-31 19:20:13 +0800

    refactor: update project routes to use validation middleware for improved schema validation

--- server/src/modules/project/project.routes.ts
+++ server/src/modules/project/project.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { protect } from '../../middleware/auth.middleware';
 import { createProject, getProjectById, getProjects, updateProject, deleteProject, restoreProject } from './project.controller';
-import { validateCreateProject } from './project.validation';
+import { createProjectSchema, getProjectSchema } from './project.validation';
+import { validate } from '../../middleware/validate.middleware';
+
 
 const router = Router();
 
-router.post("/", protect, validateCreateProject, createProject);
-router.get("/", protect, getProjects);
+router.post("/", protect, validate(createProjectSchema), createProject);
+router.get("/", protect, validate(getProjectSchema), getProjects);
 router.get("/:id", protect, getProjectById);
 router.put("/:id", protect, updateProject);
 router.delete("/:id", protect, deleteProject);
