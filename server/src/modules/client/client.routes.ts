4b81ceeaa8350916296a8d0e865d621645916562
VCNtech-123 <johnvecina640@gmail.com>
2026-07-31 14:58:57 +0800

    refactor: add zod validation for getClients route

--- server/src/modules/client/client.routes.ts
+++ server/src/modules/client/client.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { protect } from "../../middleware/auth.middleware";
 import { createClient } from "./client.controller";
-import { createClientSchema, getClientByIdSchema } from "./client.validation";
+import { createClientSchema, getClientByIdSchema, getClientsSchema } from "./client.validation";
 import { getClients, getClientById, updateClient, deleteClient } from './client.controller'
 import { validate } from "../../middleware/validate.middleware";
 
 const router = Router();
 
 router.post("/", protect, validate(createClientSchema), createClient);
-router.get("/", protect, getClients);
+router.get("/", protect, validate(getClientsSchema), getClients);
 router.get("/:id", protect, validate(getClientByIdSchema), getClientById);
 router.put("/:id", protect, updateClient);
 router.delete("/:id", protect, deleteClient);
