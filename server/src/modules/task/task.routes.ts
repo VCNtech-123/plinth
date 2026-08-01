61d410dbe09722e9bc47a1b267d0976de0ab1e9d
VCNtech-123 <johnvecina640@gmail.com>
2026-08-01 08:20:46 +0800

    refactor: add validation for getTasks route using getTasksSchema

--- server/src/modules/task/task.routes.ts
+++ server/src/modules/task/task.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { protect } from '../../middleware/auth.middleware';
-import { createProjectSchema } from './task.validation';
+import { createProjectSchema, getTasksSchema } from './task.validation';
 import { createTask, getTasks, getTaskById, updateTaskById, deleteTask, restoreTask } from './task.controller';
 import { validate } from '../../middleware/validate.middleware';
 
 const router = Router();
 
 router.post("/", protect, validate(createProjectSchema), createTask );
-router.get("/", protect, getTasks);
+router.get("/", protect, validate(getTasksSchema), getTasks);
 router.get("/:id", protect, getTaskById);
 router.put("/:id", protect, updateTaskById);
 router.delete("/:id", protect, deleteTask);
