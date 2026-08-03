e71156ebe0d96de350eef291f0d041555c66d7f1
VCNtech-123 <johnvecina640@gmail.com>
2026-08-03 10:36:47 +0800

    test: configure dotenv to load environment variables based on NODE_ENV

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,6 +1,10 @@
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({
+  path: process.env.NODE_ENV === "test"
+    ? ".env.test"
+    : ".env",
+});
 
 export const env = {
   port: process.env.PORT || 5000,
