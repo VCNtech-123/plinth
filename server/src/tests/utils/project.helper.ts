cce004a36cddf4321df2c8402f1ae5848ff31dca
VCNtech-123 <johnvecina640@gmail.com>
2026-08-03 14:08:19 +0800

    feat: add helper function to create a project for testing

---
+++ server/src/tests/utils/project.helper.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+import app from "../../app";
+
+export const createProject = async (
+  cookie: string[],
+  clientId: string
+) => {
+  const res = await request(app)
+    .post("/api/projects")
+    .set("Cookie", cookie)
+    .send({
+      name: "Test Project",
+      client: clientId,
+    });
+
+  return res.body.data.id;
+}
