c1cd46fd637de50aa303e3970e4900491238751f
VCNtech-123 <johnvecina640@gmail.com>
2026-08-03 14:06:54 +0800

    feat: add helper function to create a client for testing

---
+++ server/src/tests/utils/client.helper.ts
@@ -0,0 +1,14 @@
+import request from "supertest";
+import app from "../../app";
+
+export const createClient = async (cookie: string[]) => {
+  const res = await request(app)
+    .post("/api/clients")
+    .set("Cookie", cookie)
+    .send({
+      name: "Test Client",
+      email: "client@test.com",
+    });
+
+  return res.body.data.id;
+}
