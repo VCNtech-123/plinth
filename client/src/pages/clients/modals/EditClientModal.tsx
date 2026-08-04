ca4d71193614714144089f49a1444f1174fa2343
VCNtech-123 <johnvecina640@gmail.com>
2026-08-04 10:59:14 +0800

    feat: update import paths for DeleteClientModal and EditClientModal to use a centralized modals directory

--- client/src/pages/clients/modals/EditClientModal.tsx
+++ client/src/pages/clients/modals/EditClientModal.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import Modal from "../../components/ui/Modal";
-import Button from "../../components/ui/Button";
-import Input from "../../components/ui/Input";
+import Modal from "../../../components/ui/Modal";
+import Button from "../../../components/ui/Button";
+import Input from "../../../components/ui/Input";
 
 interface EditClientModalProps {
   open: boolean;
