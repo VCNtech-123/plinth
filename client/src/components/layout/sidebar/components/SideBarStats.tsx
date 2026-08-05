1788cc4cc7e9cb23103405684ed6faf63f028ac9
VCNtech-123 <johnvecina640@gmail.com>
2026-08-05 18:34:44 +0800

    feat: fetch and display dashboard statistics for tasks due today and overdue

--- client/src/components/layout/sidebar/components/SideBarStats.tsx
+++ client/src/components/layout/sidebar/components/SideBarStats.tsx
@@ -1,20 +1,42 @@
-// client/src/layouts/sidebar/components/SidebarStats.tsx
 import { Clock, AlertCircle } from "lucide-react";
+import { useEffect, useState } from "react";
+import { getDashboard } from "../../../../api/dashboard.api";
+
+interface DasboardSidebarSummary {
+  tasksDueToday: number,
+  overdueTasks: number
+}
 
 const SidebarStats = () => {
+
+  const [ data, setData ] = useState<DasboardSidebarSummary | null>(null)
+
+  useEffect(() => {
+    const getStatsData = async () => {
+      try {
+        const response = await getDashboard();
+        setData(response.data.summary)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getStatsData()
+  }, [])
+
   return (
     <div className="px-4 py-4 space-y-2">
       <StatItem
         icon={Clock}
         label="Due today"
-        value={5}
+        value={data?.tasksDueToday ?? 0}
         color="primary"
       />
 
       <StatItem
         icon={AlertCircle}
         label="Overdue"
-        value={2}
+        value={data?.overdueTasks ?? 0}
         color="danger"
       />
     </div>
