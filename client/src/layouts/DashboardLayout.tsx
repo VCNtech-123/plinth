afd823ea774d2f191ed784755dec02ebf0f8efd5
VCNtech-123 <johnvecina640@gmail.com>
2026-08-05 17:56:04 +0800

    refactor: improve layout structure and styling in DashboardLayout component

--- client/src/layouts/DashboardLayout.tsx
+++ client/src/layouts/DashboardLayout.tsx
@@ -1,3 +1,4 @@
+// client/src/layouts/DashboardLayout.tsx
 import { useState } from "react";
 import Sidebar from "../components/layout/sidebar/Sidebar";
 import Topbar from "../components/layout/TopBar";
@@ -5,37 +6,31 @@ import { Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
 
 const DashboardLayout = () => {
-
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
-    <div className="min-h-full bg-app text-app transition-colors">
+    <div className="flex h-screen bg-app text-app overflow-hidden">
+      {/* Sidebar - Fixed */}
+      <Sidebar
+        isOpen={isSidebarOpen}
+        onClose={() => setIsSidebarOpen(false)}
+      />
 
-      <div className="flex">
-
-        <Sidebar
-          isOpen={isSidebarOpen}
-          onClose={() => setIsSidebarOpen(false)}
+      {/* Main Content Area */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        {/* Topbar */}
+        <Topbar
+          onToggleSidebar={() => setIsSidebarOpen((prev) => !prev)}
         />
 
-        <div className="flex-1 flex flex-col min-h-screen">
-
-          {/* Topbar */}
-          <Topbar
-            onToggleSidebar={() =>
-              setIsSidebarOpen((prev) => !prev)
-            }
-          />
-
-          <main className="p-6">
+        {/* Main Content - Scrollable */}
+        <main className="flex-1 overflow-y-auto overflow-x-hidden">
+          <div className="px-6 py-8">
             <Toaster position="top-right" richColors />
             <Outlet />
-          </main>
-
-        </div>
-
+          </div>
+        </main>
       </div>
-
     </div>
   );
 };
