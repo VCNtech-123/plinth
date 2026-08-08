69c2ef3e061b3dbb62c61ddb270fa3b9e8a848e7
VCNtech-123 <johnvecina640@gmail.com>
2026-08-08 17:42:34 +0800

    feat: update getCommentsByTaskIdService to return structured response with pagination and total count

--- server/src/modules/comment/comment.service.ts
+++ server/src/modules/comment/comment.service.ts
@@ -3,7 +3,7 @@ import { Comment } from "./comment.model";
 import mongoose from "mongoose";
 import { getTaskByIdService } from '../task/task.service'
 import { CommentBody, GetCommentsQuery } from "./comment.validation";
-import { PopulatedComment } from '../../types/comment.types'
+import { GetCommentsResponse, PopulatedComment } from '../../types/comment.types'
 
 export const createCommentService = async (
     taskId: string,
@@ -32,7 +32,7 @@ export const getCommentsByTaskIdService = async (
     taskId: string,
     userId: mongoose.Types.ObjectId,
     query: GetCommentsQuery
-): Promise<PopulatedComment[] | null> => {
+): Promise<GetCommentsResponse | null> => {
 
     const { page, limit } = query;
     const skip = Math.max(0, (page - 1) * limit);
@@ -49,13 +49,20 @@ export const getCommentsByTaskIdService = async (
         return null;
     }
 
-    const comments = await Comment.find(filter)
-        .sort({ createdAt: -1 })
-        .skip(skip)
-        .limit(limit)
-        .populate("author", "name email")
-        .lean<PopulatedComment[]>()
+    const [comments, total] = await Promise.all([
+        Comment.find(filter)
+            .sort({ createdAt: -1 })
+            .skip(skip)
+            .limit(limit)
+            .populate("author", "_id name email")
+            .lean<PopulatedComment[]>(),
+        
+        Comment.countDocuments(filter)
+    ])
 
 
-    return comments;
+    return {
+        comments: comments,
+        results: total
+    };
 }
