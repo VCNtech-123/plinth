5bc3862b6569c82c8864bd0fe82c57d8b24d70b5
VCNtech-123 <johnvecina640@gmail.com>
2026-08-08 18:11:42 +0800

    feat: add deleteComment endpoint with validation to comment routes

--- server/src/modules/comment/comment.routes.ts
+++ server/src/modules/comment/comment.routes.ts
@@ -1,15 +1,15 @@
 
 import { Router } from "express";
 import { protect } from "../../middleware/auth.middleware";
-import { createComment, getCommentsByTaskId } from "./comment.controller";
+import { createComment, getCommentsByTaskId, deleteComment } from "./comment.controller";
 import { validate } from "../../middleware/validate.middleware";
-import { createCommentSchema, getCommentsSchema } from "./comment.validation";
+import { createCommentSchema, getCommentsSchema, deleteCommentSchema } from "./comment.validation";
 
 const router = Router();
 
 router.post("/tasks/:taskId/comments", protect, validate(createCommentSchema), createComment);
 router.get("/tasks/:taskId/comments", protect, validate(getCommentsSchema), getCommentsByTaskId);
-router.delete("/comments/:id", protect);
+router.delete("/comments/:id", protect, validate(deleteCommentSchema), deleteComment);
 router.patch("/comments/:id/restore", protect);
 
 export default router;
