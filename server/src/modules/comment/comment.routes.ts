03408441067618cefa7d70b2666ab03df35adfd7
VCNtech-123 <johnvecina640@gmail.com>
2026-08-08 08:19:06 +0800

    feat: protect comment routes with authentication middleware

--- server/src/modules/comment/comment.routes.ts
+++ server/src/modules/comment/comment.routes.ts
@@ -1,11 +1,12 @@
 
 import { Router } from "express";
+import { protect } from "../../middleware/auth.middleware";
 
 const router = Router();
 
-router.post("/:taskId/comments");
-router.get("/:taskId/comments");
-router.delete("/comments/:id");
-router.patch("/comments/:id/restore");
+router.post("/:taskId/comments", protect);
+router.get("/:taskId/comments", protect);
+router.delete("/comments/:id", protect);
+router.patch("/comments/:id/restore", protect);
 
 export default router;
