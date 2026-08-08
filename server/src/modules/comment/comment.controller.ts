82c9083b04774c354102d86a708a82390f6671d4
VCNtech-123 <johnvecina640@gmail.com>
2026-08-08 17:03:45 +0800

    feat: add getCommentsByTaskId endpoint and refactor createComment to improve parameter handling

--- server/src/modules/comment/comment.controller.ts
+++ server/src/modules/comment/comment.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { createCommentService } from "./comment.service";
-import { CommentBody } from "./comment.validation";
+import { createCommentService, getCommentsByTaskIdService } from "./comment.service";
+import { CommentBody, GetCommentsQuery } from "./comment.validation";
 import { ApiError } from "../../utils/ApiError";
 
 export const createComment = async (
@@ -8,13 +8,13 @@ export const createComment = async (
     res: Response
 ) => {
 
-    const id = req.params.taskId as string
+    const taskId = req.params.taskId as string
     const { body } = res.locals.validated as {
         body: CommentBody
     };
 
     const comment = await createCommentService(
-        id,
+        taskId,
         req.user!._id,
         body
     )
@@ -34,4 +34,42 @@ export const createComment = async (
             createdAt: comment.createdAt
         },
     });
+}
+
+export const getCommentsByTaskId = async (
+    req: Request,
+    res: Response
+) => {
+
+    const taskId = req.params.taskId as string;
+    const { query } = res.locals.validated as {
+        query: GetCommentsQuery
+    }
+
+    const comments = await getCommentsByTaskIdService(
+        taskId,
+        req.user!._id,
+        query
+    )
+
+    if (!comments) {
+        throw new ApiError(404, "Task not found");
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: comments.map((comment) => ({
+            id: comment._id,
+            content: comment.content,
+            task: comment.task,
+            author: {
+                name: comment.author.name,
+                email: comment.author.email
+            },
+            owner: comment.owner,
+            createdAt: comment.createdAt
+        }))
+    });
+
+    
 }
