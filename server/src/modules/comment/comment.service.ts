88e68802ecadf65b2c7266d9c5eee214703f9036
VCNtech-123 <johnvecina640@gmail.com>
2026-08-08 13:03:34 +0800

    feat: add getCommentsService to retrieve comments with pagination and filtering

--- server/src/modules/comment/comment.service.ts
+++ server/src/modules/comment/comment.service.ts
@@ -2,7 +2,7 @@
 import { Comment } from "./comment.model";
 import mongoose from "mongoose";
 import { getTaskByIdService } from '../task/task.service'
-import { CommentBody } from "./comment.validation";
+import { CommentBody, GetCommentsQuery } from "./comment.validation";
 
 export const createCommentService = async (
     taskId: string,
@@ -25,4 +25,36 @@ export const createCommentService = async (
     });
 
     return comment;
+}
+
+export const getCommentsService = async (
+    taskId: string,
+    userId: mongoose.Types.ObjectId,
+    query: GetCommentsQuery
+) => {
+
+    const { page, limit } = query;
+    const skip = Math.max(0, (page - 1) * limit);
+
+    const existingTask = await getTaskByIdService(taskId, userId);
+    const filter = {
+        owner: userId,
+        author: userId,
+        task: taskId,
+        isDeleted: false
+    }
+
+    if (!existingTask) {
+        return null;
+    }
+
+    const comments = await Comment.find(filter)
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit)
+        .populate("author", "name email")
+        .lean()
+
+
+    return comments;
 }
