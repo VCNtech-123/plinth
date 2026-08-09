f05517763638ec8cf8f5ebdf16b702457a2f368b
VCNtech-123 <johnvecina640@gmail.com>
2026-08-09 20:10:15 +0800

    feat: refactor registerUser to use session management and create default workspace

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { User } from "../user/user.model";
 import { ApiError } from '../../utils/ApiError';
 import { Types } from 'mongoose'
+import { createDefaultWorkspaceForUser } from '../workspace/workspace.service'
+import mongoose from "mongoose";
 
 export const registerUser = async ( 
     name: string,
@@ -13,13 +15,30 @@ export const registerUser = async (
         throw new ApiError(401, 'Email already registered!');
     }
 
-    const user = await User.create({
-        name,
-        email,
-        password
-    });
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
-    return user;
+    try {
+        const user = await User.create(
+        [{ name, email, password }],
+        { session }
+        );
+
+        await createDefaultWorkspaceForUser(
+        user[0]._id,
+        user[0].name,
+        session
+        );
+
+        await session.commitTransaction();
+        session.endSession();
+
+        return user[0];
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        throw error;
+    }
 }
 
 export const loginUser = async (
