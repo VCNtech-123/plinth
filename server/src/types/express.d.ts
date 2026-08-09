b7de8c65d30885b54366cbdda5fb3592028582a1
VCNtech-123 <johnvecina640@gmail.com>
2026-08-09 20:52:45 +0800

    feat: extend Express Request interface to include workspace and membership types

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,9 +1,13 @@
 import { IUser } from "../modules/user/user.model";
+import { IWorkspace } from "../modules/workspace/workspace.model";
+import { IWorkspaceMember } from "../modules/workspace/workspaceMember.model";
 
 declare global {
     namespace Express {
         interface Request {
             user?: IUser;
+            workspace?: IWorkspace;
+            membership?: IWorkspaceMember;
         }
     }
 }
