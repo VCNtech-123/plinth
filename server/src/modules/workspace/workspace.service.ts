36cd03c8528e42b67136619c179cf3b2dc865816
VCNtech-123 <johnvecina640@gmail.com>
2026-08-11 13:37:03 +0800

    feat: add addWorkspaceMemberService to manage workspace member invitations

--- server/src/modules/workspace/workspace.service.ts
+++ server/src/modules/workspace/workspace.service.ts
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import { Workspace } from "./workspace.model";
-import { WorkspaceMember } from "./workspaceMember.model";
+import { WorkspaceMember, IWorkspaceMember, WorkspaceRole } from "./workspaceMember.model";
 import { WorkspaceResponse, PopulatedWorkspace, PopulatedMember } from "../../types/workspace.types";
+import { findUserByEmailService } from "../auth/auth.service";
+import { ApiError } from "../../utils/ApiError";
 
 export const createDefaultWorkspaceForUser = async (
   userId: mongoose.Types.ObjectId,
@@ -67,3 +69,38 @@ export const getWorkspaceMembersService = async (
 
   return members
 }
+
+  export const addWorkspaceMemberService = async (
+    workspaceId: mongoose.Types.ObjectId,
+    email: string,
+    role: WorkspaceRole,
+    requesterMembership: IWorkspaceMember
+  ) => {
+
+      if (requesterMembership.role === "admin" && (role === "admin" || role === "owner")) {
+        throw new ApiError(403, "Admins can only invite members and viewers");
+      }
+      
+      const userToAdd = await findUserByEmailService(email);
+
+      if (role === 'admin' || role === "owner") {
+        throw new ApiError(403, "Admins can only create members and viewers")
+      }
+
+      const existingMember = await WorkspaceMember.findOne({
+        workspace: workspaceId,
+        user: userToAdd._id
+      });
+
+      if (existingMember) {
+        throw new ApiError(400, "User is already a member of this workspace");
+      }
+
+      const addedUser = await WorkspaceMember.create({
+        workspace: workspaceId,
+        user: userToAdd._id,
+        role: role
+      })
+
+      return addedUser
+  }
