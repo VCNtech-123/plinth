2a3ecdeb1078e5b27ab83a0337a0cc7c75b09eb2
VCNtech-123 <johnvecina640@gmail.com>
2026-08-11 12:36:53 +0800

    feat: add getWorkspaceMembersService to retrieve members of a workspace

--- server/src/modules/workspace/workspace.service.ts
+++ server/src/modules/workspace/workspace.service.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { Workspace } from "./workspace.model";
 import { WorkspaceMember } from "./workspaceMember.model";
-import { WorkspaceResponse, PopulatedWorkspace } from "../../types/workspace.types";
+import { WorkspaceResponse, PopulatedWorkspace, PopulatedMember } from "../../types/workspace.types";
 
 export const createDefaultWorkspaceForUser = async (
   userId: mongoose.Types.ObjectId,
@@ -53,4 +53,17 @@ export const getCurrentWorkspaceService = async (
     role: membership.role,
     membersCount
   }
+}
+
+export const getWorkspaceMembersService = async (
+  workspaceId: mongoose.Types.ObjectId
+): Promise<PopulatedMember[]> => {
+
+  const members = await WorkspaceMember.find({
+    workspace: workspaceId
+  }).populate("user", "_id name email")
+    .select("role joinedAt user")
+    .lean<PopulatedMember[]>()
+
+  return members
 }
