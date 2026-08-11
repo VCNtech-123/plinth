fd330957b063a58895e60a8a2450d4ebb5a60b41
VCNtech-123 <johnvecina640@gmail.com>
2026-08-11 12:01:43 +0800

    feat: add workspace types for PopulatedWorkspace, LeanWorkspace, and WorkspaceResponse

---
+++ server/src/types/workspace.types.ts
@@ -0,0 +1,23 @@
+
+import { IWorkspaceMember, WorkspaceRole } from "../modules/workspace/workspaceMember.model";
+import { Types } from "mongoose";
+
+export type PopulatedWorkspace = Omit<IWorkspaceMember, "workspace"> & {
+  workspace: {
+    _id: Types.ObjectId;
+    name: string;
+    createdBy: string;
+  };
+};
+
+export interface LeanWorkspace {
+  _id: Types.ObjectId;
+  name: string;
+  createdBy: string;
+}
+
+export interface WorkspaceResponse { 
+    workspace: LeanWorkspace;
+    role: WorkspaceRole;
+    membersCount: number;
+}
