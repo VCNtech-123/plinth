5b8d56b693a10fe906f841d74193fdd0485c2afe
VCNtech-123 <johnvecina640@gmail.com>
2026-08-11 12:37:06 +0800

    feat: implement getWorkspaceMembers controller to retrieve members of a workspace

--- server/src/modules/workspace/workspace.controller.ts
+++ server/src/modules/workspace/workspace.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { getCurrentWorkspaceService } from './workspace.service'
+import { getCurrentWorkspaceService, getWorkspaceMembersService } from './workspace.service'
 import { ApiError } from '../../utils/ApiError'
 
 export const getCurrentWorkspace = async ( 
@@ -29,5 +29,30 @@ export const getCurrentWorkspace = async (
             role,
             membersCount
         }
+    })
+}
+
+export const getWorkspaceMembers = async (
+    req: Request,
+    res: Response
+) => {
+
+    const members = await getWorkspaceMembersService(req.workspace!._id)
+
+    if (!members || members.length === 0) {
+        throw new ApiError(404, "No members found")
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: members.map((member) => ({
+            user: {
+                id: member.user._id,
+                name: member.user.name,
+                email: member.user.email
+            },
+            role: member.role,
+            joinedAt: member.joinedAt
+        }))
     })
 }
