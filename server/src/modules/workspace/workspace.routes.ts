d369c5b1f74dc47b55d1b0862cc158ee6a3f4062
VCNtech-123 <johnvecina640@gmail.com>
2026-08-11 15:39:42 +0800

    feat: add routes for workspace invites and accepting invites

--- server/src/modules/workspace/workspace.routes.ts
+++ server/src/modules/workspace/workspace.routes.ts
@@ -2,15 +2,18 @@
 import { Router } from "express";
 import { protect } from "../../middleware/auth.middleware";
 import { attachWorkspace } from "../../middleware/workspace.middleware";
-import { getCurrentWorkspace, getWorkspaceMembers, addWorkspaceMember } from "./workspace.controller";
+import { getCurrentWorkspace, getWorkspaceMembers, addWorkspaceMember, getInvites, acceptInvite } from "./workspace.controller";
 import { validate } from "../../middleware/validate.middleware";
-import { addWorkspaceMemberSchema } from "./workspace.validation";
+import { addWorkspaceMemberSchema, acceptInviteSchema } from "./workspace.validation";
 
 const router = Router()
 
 router.use(protect, attachWorkspace)
 router.get("/me", getCurrentWorkspace);
 router.get("/me/members", getWorkspaceMembers);
-router.post("/me/members", validate(addWorkspaceMemberSchema), addWorkspaceMember)
+router.get("/me/invites", getInvites);
+router.post("/me/members", validate(addWorkspaceMemberSchema), addWorkspaceMember);
+router.patch("/me/:id/accept-invite", validate(acceptInviteSchema), acceptInvite)
+
 
 export default router;
