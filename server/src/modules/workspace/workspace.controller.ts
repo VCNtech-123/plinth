16bfb848576e247e117ac3ee0ade786e0c3bc139
VCNtech-123 <johnvecina640@gmail.com>
2026-08-11 15:39:37 +0800

    feat: add getInvites and acceptInvite functions to handle workspace invitations

--- server/src/modules/workspace/workspace.controller.ts
+++ server/src/modules/workspace/workspace.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { getCurrentWorkspaceService, getWorkspaceMembersService, addWorkspaceMemberService } from './workspace.service'
+import { getCurrentWorkspaceService, getWorkspaceMembersService, addWorkspaceMemberService, getInvitesService, acceptInviteService } from './workspace.service'
 import { ApiError } from '../../utils/ApiError'
 import { MemberBody } from './workspace.validation'
 
@@ -40,7 +40,7 @@ export const getWorkspaceMembers = async (
 
     const members = await getWorkspaceMembersService(req.workspace!._id)
 
-    if (!members || members.length === 0) {
+    if (!members) {
         throw new ApiError(404, "No members found")
     }
 
@@ -91,3 +91,43 @@ export const addWorkspaceMember = async (
         }
     )
 }   
+
+export const getInvites = async (
+    req: Request,
+    res: Response 
+) => {
+
+    const invites = await getInvitesService(req.user!._id);
+
+    res.status(200).json({
+        status: "success",
+        data: invites.map((invite) => ({
+                id: invite.workspace._id,
+                name: invite.workspace.name,
+                createdBy: invite.workspace.createdBy
+        }))
+    })
+}
+
+export const acceptInvite = async (
+    req: Request,
+    res: Response 
+) => {
+
+    const id = req.params.id as string
+    const workspace = await acceptInviteService(id, req.user!._id);
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            user: {
+                id: workspace.user._id,
+                name: workspace.user.name,
+                email: workspace.user.email
+            },
+            role: workspace.role,
+            joinedAt: workspace.joinedAt
+            }
+        }
+    )
+}
